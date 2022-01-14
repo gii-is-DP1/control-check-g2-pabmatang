a22ad3ddc1723f7b350fadc2d818e363a0f1b3af
pabmatang <pablo-mateos@hotmail.es>
2022-01-14 12:19:20 +0100

    Los 7 primeros test correctos

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,16 +2,46 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+@Table(name = "Feeding")
+public class Feeding extends BaseEntity{
+	
+	@NotNull
+	@Column(name = "start_date")   
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate startDate;
+	
+	@NotNull
+	@Min(1)
+	@Column(name = "weeks_duration") 
     double weeksDuration;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
     Pet pet;   
+	
+	@ManyToOne()
+	@JoinColumn(name = "FeedingType")
+    private FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,28 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
+	@Autowired
+	private FeedingRepository feedingRepository;
+	
     public List<Feeding> getAll(){
-        return null;
+    	return feedingRepository.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+    	 return feedingRepository.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return feedingRepository.findByName(typeName);
     }
 
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+        return feedingRepository.save(p);       
     }
 
     
